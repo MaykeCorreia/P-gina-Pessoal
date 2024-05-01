0b4049531f9e32789fec5da62ef1a54a76dee673
MaykeCorreia <mayke_correia@hotmail.com>
2024-05-01 19:29:10 -0300

    Javascript functions

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+ function carregarTextoSobre() {
+        let div = document.getElementById("textoSobre");
+        
+
+        div.innerHTML = "<h5>Informações Sobre Mim</h5><p>Me Chamo Mayke Correia.</p><p>Sempre buscando excelência em todas minhas atividades desenvolvidas na indústria,</p><p>me qualificando e trazendo resultados a todas as tarefas a mim designadas.</p><p>Atualmente atuando no Pós Venda e atendimento ao cliente.</p><p>Estudante do 3° Período de Engenharia de Software na universidade Unicesumar</p><p>e formado como técnico em Mecatrônica na instituição Senai-Pr.</p>";
+    }
