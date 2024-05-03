44f5d2e5d60dde2599268688767c6d496ac768ec
MaykeCorreia <mayke_correia@hotmail.com>
2024-05-02 22:59:41 -0300

    implementações

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@
         let div = document.getElementById("textoSobre");
         
 
-        div.innerHTML = "<h5>Informações Sobre Mim</h5><p>Me Chamo Mayke Correia, e esta é uma breve descrição sobre quem sou.</p><p>Sempre buscando excelência em todas minhas atividades desenvolvidas na indústria,</p><p>me qualificando e trazendo resultados a todas as tarefas a mim designadas.</p><p>Atualmente atuando no Pós Venda e atendimento ao cliente.</p><p>Estudante do 3° Período de Engenharia de Software na universidade UNICESUMAR</p><p>e formado como técnico em Mecatrônica pela instituição SENAI-PR.</p>";
+        div.innerHTML = "<h5>Informações Sobre Mim</h5><p>Me Chamo Mayke Correia, e esta é uma breve descrição sobre quem sou.</p><p>Sempre buscando excelência em todas minhas atividades desenvolvidas na indústria,</p><p>me qualificando e trazendo resultados a todas as tarefas a mim designadas.</p><p>Atualmente atuando no Pós Venda e atendimento ao cliente.</p><p>Estudante do 3° Período de Engenharia de Software na universidade UNICESUMAR</p><p>e formado como técnico em Mecatrônica pela instituição SENAI-PR.</p><p>Habilidades em HTML, CSS, JavaScript e Linguagem C;</p><p>Vasta Experiência nas áreas de mecânica industrial, elétrica industrial, hidráulica e pneumática.</p>";
     }
